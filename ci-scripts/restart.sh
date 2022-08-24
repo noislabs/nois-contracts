bd2beb293493857069dc854f17f5831e486d3a33
Simon Warta <simon@warta.it>
2022-08-24 11:28:04 +0200

    Add sleep to restart script

--- ci-scripts/restart.sh
+++ ci-scripts/restart.sh
@@ -7,6 +7,9 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 docker kill osmosis || true
 docker kill wasmd || true
 
+# Ensure all volumes are freed
+sleep 0.5
+
 "$SCRIPT_DIR"/osmosis/start.sh > /dev/null &
 "$SCRIPT_DIR"/wasmd/start.sh > /dev/null &
 
