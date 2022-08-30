14f9612d68ceaca58ee3e163fb8f8e42f64dff34
Simon Warta <simon@warta.it>
2022-08-31 01:50:22 +0200

    Add shuffle implementation

--- packages/nois-toolbox/src/lib.rs
+++ packages/nois-toolbox/src/lib.rs
@@ -1,8 +1,10 @@
 mod coinflip;
 mod decimal;
 mod prng;
+mod shuffle;
 mod sub_randomness;
 
 pub use crate::coinflip::{coinflip, Side};
 pub use crate::decimal::random_decimal;
+pub use crate::shuffle::shuffle;
 pub use crate::sub_randomness::{sub_randomness, SubRandomnessProvider};
