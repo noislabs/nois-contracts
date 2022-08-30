e2a4e674a047678931c094ea99e09265a1d37cb6
Simon Warta <simon@warta.it>
2022-08-31 00:45:48 +0200

    Add coinflip

--- packages/nois-toolbox/src/lib.rs
+++ packages/nois-toolbox/src/lib.rs
@@ -1,6 +1,8 @@
+mod coinflip;
 mod decimal;
 mod prng;
 mod sub_randomness;
 
+pub use crate::coinflip::{coinflip, Side};
 pub use crate::decimal::random_decimal;
 pub use crate::sub_randomness::{sub_randomness, SubRandomnessProvider};
