85facdb4ecb39e86fd368e6b0754154c89b2fd70
Kais <kais@aidence.com>
2022-09-05 22:32:46 +0100

    less-hardcode

--- cd-scripts/deploy-customer-chain.sh
+++ cd-scripts/deploy-customer-chain.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-export NODE_URL=https://rpc.uni.juno.deuslabs.fi:443
+NODE_URL=https://rpc.uni.juno.deuslabs.fi:443
+BINARY_NAME=junod
+DENOM=ujunox
+FEES=1000000
 
 echo "Store proxy"
-export NOIS_PROXY_CODE_ID=$(junod tx wasm store artifacts/nois_proxy.wasm --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=1000000ujunox --broadcast-mode=block --node=$NODE_URL -y |yq -r ".logs[0].events[1].attributes[0].value")
+NOIS_PROXY_CODE_ID=$($BINARY_NAME tx wasm store artifacts/nois_proxy.wasm --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=$FEES$DENOM --broadcast-mode=block --node=$NODE_URL -y |yq -r ".logs[0].events[1].attributes[0].value")
 
 echo "Instantiate proxy"
-export NOIS_PROXY_CONTRACT_ADDRESS=$(junod tx wasm instantiate $NOIS_PROXY_CODE_ID '{}'  --label=nois-proxy --no-admin --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=1000000ujunox --broadcast-mode=block --node=$NODE_URL  -y |yq -r '.logs[0].events[0].attributes[0].value' )
+NOIS_PROXY_CONTRACT_ADDRESS=$($BINARY_NAME tx wasm instantiate $NOIS_PROXY_CODE_ID '{}'  --label=nois-proxy --no-admin --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=$FEES$DENOM --broadcast-mode=block --node=$NODE_URL  -y |yq -r '.logs[0].events[0].attributes[0].value' )
 echo "NOIS_PROXY_CONTRACT_ADDRESS: $NOIS_PROXY_CONTRACT_ADDRESS"
  
-
 echo "Store demo"
-export NOIS_DEMO_CODE_ID=$(junod tx wasm store artifacts/nois_demo.wasm --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=1000000ujunox --broadcast-mode=block --node=$NODE_URL -y |yq -r ".logs[0].events[1].attributes[0].value")
+NOIS_DEMO_CODE_ID=$($BINARY_NAME tx wasm store artifacts/nois_demo.wasm --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=$FEES$DENOM --broadcast-mode=block --node=$NODE_URL -y |yq -r ".logs[0].events[1].attributes[0].value")
  
-
-
 echo "Instantiate demo"
-export NOIS_DEMO_CONTRACT_ADDRESS=$(junod tx wasm instantiate $NOIS_DEMO_CODE_ID '{"nois_proxy": "'"$NOIS_PROXY_CONTRACT_ADDRESS"'"}'  --label=nois-demo --no-admin --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=1000000ujunox --broadcast-mode=block --node=$NODE_URL -y |yq -r '.logs[0].events[0].attributes[0].value' )
+NOIS_DEMO_CONTRACT_ADDRESS=$($BINARY_NAME tx wasm instantiate $NOIS_DEMO_CODE_ID '{"nois_proxy": "'"$NOIS_PROXY_CONTRACT_ADDRESS"'"}'  --label=nois-demo --no-admin --from juno-key --chain-id uni-3   --gas=auto --gas-adjustment 1.4  --fees=$FEES$DENOM --broadcast-mode=block --node=$NODE_URL -y |yq -r '.logs[0].events[0].attributes[0].value' )
 echo "NOIS_DEMO_CONTRACT_ADDRESS: $NOIS_DEMO_CONTRACT_ADDRESS"
