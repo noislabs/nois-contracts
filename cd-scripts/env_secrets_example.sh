94e50bc9d5a5354d0e5ca08777abc1268b3dd24c
Kais <kais@aidence.com>
2022-09-08 21:41:52 +0100

    state-file

--- cd-scripts/env_secrets_example.sh
+++ cd-scripts/env_secrets_example.sh
@@ -2,4 +2,4 @@
 export RELAYER_MNEMONIC="your seed goes here"
 export GITHUB_OAUTH_TOKEN="ghp_XXXX" 
 export DISCORD_WEBHOOK="https://discord.com/api/webhooks/......."
-export CHAIN_MNEMONIC="you inquiry anxiety .................."
+export MNEMONIC="you inquiry anxiety .................."
