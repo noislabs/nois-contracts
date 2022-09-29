9b20bda3e5eb14fc8c39693521d6c6b02b2607fb
Kais <kais@aidence.com>
2022-09-29 16:19:39 +0100

    adapt-perf-test

--- cd-scripts/test-perf.sh
+++ cd-scripts/test-perf.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-
+set -x
 #PREREQS
 # 0 You have already run the deploy-contracts script
 
 SCRIPT_DIR="cd-scripts"
+KEYRING_KEY_NAME="deployment-key"
+
 cd $SCRIPT_DIR
-. config.sh
 
+chain=elgafar-1
+contract=nois-demo
 
-NOIS_DEMO_CONTRACT_ADDRESS=$(cat generated_$CHAIN_ID.json |jq -r '."'"$CHAIN_ID"'"[]| select(.contract_name=="nois-demo").address ' )
+NOIS_DEMO_CONTRACT_ADDRESS=$(cat config.yaml|yq -r '.chains[]| select(.name=="'"$chain"'").wasm.contracts[]| select(.name=="'"$contract"'").address' )
+BINARY_NAME=($(yq -r '.chains[]| select(.name=="'"$chain"'").binary_name' config.yaml))
+NODE_URL=($(yq -r '.chains[]| select(.name=="'"$chain"'").rpc[0]' config.yaml))
+CHAIN_ID=($(yq -r '.chains[]| select(.name=="'"$chain"'").chain_id' config.yaml))
+FAUCET_URL=($(yq -r '.chains[]| select(.name=="'"$chain"'").faucet' config.yaml))
+DENOM=($(yq -r '.chains[]| select(.name=="'"$chain"'").denom' config.yaml))
+GAS_PRICES=($(yq -r '.chains[]| select(.name=="'"$chain"'").gas_price' config.yaml))
 
-#$BINARY_NAME tx wasm execute artifacts/nois_demo.wasm --from $LOCAL_KEYRING_KEY --chain-id $CHAIN_ID   --gas=auto --gas-adjustment 1.4  --fees=$FEES$DENOM --broadcast-mode=block --node=$NODE_URL -y
+if [ "$FAUCET_URL" == "~" ] ;
+        then echo "$chain : Info: Faucet is not relevant here";
+        else echo "$chain : Trying to add credit for chain '$CHAIN_ID' with faucet '$FAUCET_URL'";
+          BECH_ADDR=$($BINARY_NAME keys show $KEYRING_KEY_NAME -a ) 
+          curl -XPOST -H 'Content-type: application/json' -d "{\"address\":\"$BECH_ADDR\",\"denom\":\"$DENOM\"}" $FAUCET_URL
+          echo "$chain - $contract : querying new balance ..."
+          $BINARY_NAME query bank balances $BECH_ADDR --node=$NODE_URL | yq -r '.balances' 
+    fi
+
+#$BINARY_NAME tx wasm execute artifacts/nois_demo.wasm --from $KEYRING_KEY_NAME --chain-id $CHAIN_ID   --gas=auto --gas-adjustment 1.4  --fees=$FEES$DENOM --broadcast-mode=block --node=$NODE_URL -y
 declare -i TTL
 TTL='200'
 
@@ -19,7 +37,7 @@ do
    timestamp=$(date +%s)
    result="null"
    echo "$timestamp"
-   $BINARY_NAME tx wasm execute $NOIS_DEMO_CONTRACT_ADDRESS  '{"estimate_pi": {"job_id": "'"$timestamp"'"}}'  --from $LOCAL_KEYRING_KEY --chain-id $CHAIN_ID   --gas=auto --gas-adjustment 1.4  --gas-prices=$GAS_PRICES$DENOM --broadcast-mode=block --node=$NODE_URL -y >/dev/null
+   $BINARY_NAME tx wasm execute $NOIS_DEMO_CONTRACT_ADDRESS  '{"estimate_pi": {"job_id": "'"$timestamp"'"}}'  --from $KEYRING_KEY_NAME --chain-id $CHAIN_ID   --gas=auto --gas-adjustment 1.4  --gas-prices=$GAS_PRICES$DENOM --broadcast-mode=block --node=$NODE_URL -y >/dev/null
    SECONDS=0
    i=0
    while [ "$result" == "null" ] && [ "$i" -lt "$TTL" ] 
