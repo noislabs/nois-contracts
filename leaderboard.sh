ce24694069a2836376860bac45e5850b4655e597
Kais baccour <baccour.kais@yahoo.fr>
2022-10-13 23:18:01 +0200

    remove-nesting

--- leaderboard.sh
+++
@@ -1,6 +0,0 @@
-number_of_bots=$(wasmd query wasm  contract-state  smart nois1j7m4f68lruceg5xq3gfkfdgdgz02vhvlq2p67vf9v3hwdydaat3sajzcy5  '{"bots": {}}'  --node=http://node-0.noislabs.com:26657 |yq -r '.data.bots | length')
-for bot in `seq 0 $(($number_of_bots - 1))` 
- do
-  wasmd query wasm  contract-state  smart nois1j7m4f68lruceg5xq3gfkfdgdgz02vhvlq2p67vf9v3hwdydaat3sajzcy5  '{"bots": {}}'  --node=http://node-0.noislabs.com:26657 |yq -r ".data.bots[$bot].moniker"
-  wasmd query wasm  contract-state  smart nois1j7m4f68lruceg5xq3gfkfdgdgz02vhvlq2p67vf9v3hwdydaat3sajzcy5  '{"bots": {}}'  --node=http://node-0.noislabs.com:26657 |yq -r ".data.bots[$bot].rounds_added"
-done
