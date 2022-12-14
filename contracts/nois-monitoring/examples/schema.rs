618c5b8813a192da614e5377029d859931959be1
Simon Warta <simon@warta.it>
2022-12-14 23:39:40 +0100

    Fix  import

--- contracts/nois-monitoring/examples/schema.rs
+++ contracts/nois-monitoring/examples/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use monitoring::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use nois_monitoring::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
