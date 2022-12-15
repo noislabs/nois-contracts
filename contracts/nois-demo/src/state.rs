269e995eeb2271f8ed031c9ba4c294cc70379352
Kais <baccour.kais@yahoo.fr>
2022-12-15 03:02:59 +0100

    remove-unnecessary-change

--- contracts/nois-demo/src/state.rs
+++ contracts/nois-demo/src/state.rs
@@ -6,12 +6,3 @@ pub const NOIS_PROXY: Item<Addr> = Item::new("nois_proxy");
 
 /// Map from callback ID to result
 pub const RESULTS: Map<&str, Decimal> = Map::new("results");
-
-/// The blocks when randomness was requested and received. This is used for performance metrics purposes
-#[cw_serde]
-pub struct RandomnessLifecycleBlocks {
-    pub request_block: String,
-    pub channel: String,
-    pub deliver_block: String,
-    pub job_id: String,
-}
