5cda5770964ecaaf1c7a6ebecc57a3192e54e0d6
Simon Warta <simon@warta.it>
2023-03-26 15:24:18 +0200

    Update sink and icecube address for 0.11.0 checksum

--- packages/misc/src/mainnet_addresses.rs
+++ packages/misc/src/mainnet_addresses.rs
@@ -8,9 +8,9 @@ const PREFIX: &str = "nois";
 const SALT: &[u8] = b"\x01";
 
 fn icecube_address() -> String {
-    // See `curl -L https://github.com/noislabs/nois-contracts/releases/download/v0.10.0/checksums.txt`
+    // See `curl -L https://github.com/noislabs/nois-contracts/releases/download/v0.11.0/checksums.txt | grep icecube`
     let checksum =
-        hex::decode("2856fea75fd9de874e85e835f8de2372532694f1b310a7ffe341be9e16ce183a").unwrap();
+        hex::decode("b8cc8ae4201ee757a70c1e6d8b72c3afb94fc36ba95294ab0614011069de0fe5").unwrap();
 
     let (_, creator_data, variant) = bech32::decode(DEPLOYMENTS).unwrap();
     let creator: CanonicalAddr = Vec::<u8>::from_base32(&creator_data).unwrap().into();
@@ -21,9 +21,9 @@ fn icecube_address() -> String {
 }
 
 fn sink_address() -> String {
-    // See `curl -L https://github.com/noislabs/nois-contracts/releases/download/v0.10.0/checksums.txt`
+    // See `curl -L https://github.com/noislabs/nois-contracts/releases/download/v0.11.0/checksums.txt | grep sink`
     let checksum =
-        hex::decode("6d0b1b81c2ae10b0772e22041ff179d82b67a50cc2dc9a645728666ef96a9682").unwrap();
+        hex::decode("cb7b021831dd908b673491288b7a76f7ae973b8c1586ce41d9a69cadab5202c7").unwrap();
 
     let (_, creator_data, variant) = bech32::decode(DEPLOYMENTS).unwrap();
     let creator: CanonicalAddr = Vec::<u8>::from_base32(&creator_data).unwrap().into();
@@ -42,7 +42,7 @@ mod tests {
         let addr = icecube_address();
         assert_eq!(
             addr,
-            "nois1mdjzz7y7crnjgfkdld4x9ravqymh22jy2c7jpukqnkghhzv5s9ys2a9hu7"
+            "nois1gwnfyx82rwgc4y9r8vx6nr9v35dwezw3dadw6h39mad9amg7shnsler5f0"
         );
     }
 
@@ -51,7 +51,7 @@ mod tests {
         let addr = sink_address();
         assert_eq!(
             addr,
-            "nois16rpxzry8jf06j8htjphf0lnmylh28enwm4xwkfpn3699wvyys2yqgcev3h"
+            "nois10c0ppz0n57hqrmfp7g7lqs6k4xk9rxhvcfkqt83r8mars2lc57mq0f6cty"
         );
     }
 }
