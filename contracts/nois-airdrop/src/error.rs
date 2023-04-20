65682084a9fda084a12e24fb37d1c51b730757bb
Kais <baccour.kais@yahoo.fr>
2023-04-19 22:25:55 -0300

    make-it-a-randdrop

--- contracts/nois-airdrop/src/error.rs
+++ contracts/nois-airdrop/src/error.rs
@@ -14,8 +14,14 @@ pub enum ContractError {
     #[error("Unauthorized.")]
     Unauthorized,
 
+    #[error("Proxy address is not valid")]
+    InvalidProxyAddress,
+
     #[error("Merkle is immutable.")]
     MerkleImmutable,
+
+    #[error("Register the Merkle root before requesting randomness")]
+    MerkleRootAbsent,
 
     #[error("Invalid input")]
     InvalidInput {},
@@ -29,6 +35,23 @@ pub enum ContractError {
     #[error("Verification failed")]
     VerificationFailed {},
 
+    #[error("The sender is not randomly eligible for the randdrop")]
+    NotRandomlyEligible {},
+
+    #[error("The claiming phase didnot start. The random beacon is yet to be fetched")]
+    RandomnessUnavailable {},
+
     #[error("Cannot migrate from different contract type: {previous_contract}")]
     CannotMigrate { previous_contract: String },
+
+    // callback should only be allowed to be called by the proxy contract
+    // otherwise anyone can cut the randomness workflow and cheat the randomness
+    #[error("Unauthorized Receive execution")]
+    UnauthorizedReceive,
+
+    #[error("Received invalid randomness")]
+    InvalidRandomness,
+
+    #[error("Immutable Randomness")]
+    ImmutableRandomness,
 }
