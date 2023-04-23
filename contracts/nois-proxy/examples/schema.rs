1e25d399f0cf2ba1094c35e3c5e5dccb7bfdc6fd
Simon Warta <simon@warta.it>
2023-04-23 18:19:07 +0200

    Add SudoMsg to schema

--- contracts/nois-proxy/examples/schema.rs
+++ contracts/nois-proxy/examples/schema.rs
@@ -1,11 +1,12 @@
 use cosmwasm_schema::write_api;
 
-use nois_proxy::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use nois_proxy::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, SudoMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         query: QueryMsg,
         execute: ExecuteMsg,
+        sudo: SudoMsg,
     }
 }
